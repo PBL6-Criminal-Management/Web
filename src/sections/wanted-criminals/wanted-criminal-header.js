fe5fb725f833e438c5f96629d8e652a51cec0d73
Nhan <thnhantr@gmail.com>
2023-12-27 01:37:57 +0700

    landing page

--- src/sections/wanted-criminals/wanted-criminal-header.js
+++ src/sections/wanted-criminals/wanted-criminal-header.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+import { AppBar, Toolbar, Typography, Box, Button } from '@mui/material';
 import { Logo } from 'src/components/logo';
+import { useRouter } from 'next/router';
 
 const Header = () => {
+  const router = useRouter();
+  const handleLoginClick = () => {
+    router.push('/auth/login');
+  };
+
   return (
     <AppBar position="static" sx={{ backgroundColor: '#1C2536' }}>
       <Toolbar>
@@ -20,6 +26,11 @@ const Header = () => {
         <Typography variant="h6" component="div">
           TRANG THÔNG TIN TRUY NÃ
         </Typography>
+        <Box sx={{ marginLeft: 'auto' }}>
+          <Button color="inherit" onClick={handleLoginClick}>
+            Login
+          </Button>
+        </Box>
       </Toolbar>
     </AppBar>
   );
